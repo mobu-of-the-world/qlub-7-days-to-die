8ade86c3172d83dd54c3c683e4bfc06622fac030
ujihisa <ujihisa@users.noreply.github.com>
2021-12-25 19:54:21 -0800

    Integrate with discord

---
+++ discord_notify_bot.rb
@@ -0,0 +1,30 @@
+require 'uri'
+require 'net/http'
+require 'json'
+
+def discord(msg)
+  Net::HTTP.post(
+    URI.parse(ENV.fetch('DISCORD_WEBHOOK_URL')),
+    { content: msg.to_s }.to_json,
+    { 'Content-Type' => 'application/json' },
+  )
+end
+
+discord('Server restarted')
+
+IO.popen('tail -f /7-days-to-die/output.txt') do |io|
+  loop do
+    line = io.readline.chomp
+    case line
+    when %r(^[^ ]+ [^ ]+ INF RequestToEnterGame: [^/]+/(.+)$)
+      # 2021-12-26T03:34:56 72.400 INF RequestToEnterGame: EOS_0002831795af4125b164741191188cab/ujihisa
+      discord("#{$1} is trying to join")
+    when %r(^[^ ]+ [^ ]+ INF GMSG: Player '(.+)' joined the game$)
+      # 2021-12-26T03:35:40 115.999 INF GMSG: Player 'ujihisa' joined the game
+      discord("#{$1} joined the game")
+    when %r(^[^ ]+ [^ ]+ INF GMSG: Player '(.+)' left the game$)
+      # 2021-12-26T03:35:46 121.808 INF GMSG: Player 'ujihisa' left the game
+      discord("#{$1} left the game")
+    end
+  end
+end
